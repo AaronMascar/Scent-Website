e6a1889ab522eb25784f05325b85ed384560675a
AaronMascar <148704716+AaronMascar@users.noreply.github.com>
2026-08-05 19:41:05 +0400

    update

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,21 +1,26 @@
-var assets = [
-	"/".
-	"/index.html",
-	"/js/app.js"
-]
+const CACHE_NAME = "scent-perfumes-v1";
+const assets = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./Home/nav.css",
+  "./manifest.json",
+  "./icons/icon-192x192.png",
+  "./icons/icon-512x512.png"
+];
 
 self.addEventListener("install", function(installEvent) {
-	installEvent.waitUntil(
-		caches.open("my-test-pwa").then(function(cache) {
-			cache.addAll(assets)
-		})
-	)
-})
+  installEvent.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(assets);
+    })
+  );
+});
 
 self.addEventListener("fetch", function(fetchEvent) {
-	fetchEvent.respondWith(
-		caches.match(fetchEvent.request).then(function(res) {
-		return res || fetch(fetchEvent.request)
-		})
-	)
-})
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(function(res) {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
